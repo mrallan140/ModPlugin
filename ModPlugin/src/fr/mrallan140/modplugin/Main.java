51b86db167fa2935471465355511f46762e97fbc
mrallan140 <allan2000@wanadoo.fr>
2017-01-23 22:48:53 +0100

    FIRST COMMIT

---
+++ ModPlugin/src/fr/mrallan140/modplugin/Main.java
@@ -0,0 +1,15 @@
+package fr.mrallan140.modplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable(){
+        System.out.println("Plugin Moderation ON !");
+    }
+    
+    public void onDisable(){
+        System.out.println("Plugin Moderation OFF !");
+    }
+            
+}
