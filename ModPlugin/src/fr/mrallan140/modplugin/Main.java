7c2f33802b477fa147346f08eb55cccc506b4bb1
mrallan140 <allan2000@wanadoo.fr>
2017-01-24 08:04:20 +0100

    FIRST COMMIT

--- ModPlugin/src/fr/mrallan140/modplugin/Main.java
+++ ModPlugin/src/fr/mrallan140/modplugin/Main.java
@@ -1,5 +1,6 @@
 package fr.mrallan140.modplugin;
 
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -11,5 +12,10 @@ public class Main extends JavaPlugin {
     public void onDisable(){
         System.out.println("Plugin Moderation OFF !");
     }
+    
+    public void onPlayerJoin(PlayerJoinEvent e) {
+       e.getJoinMessage();
+    }
+    
             
 }
