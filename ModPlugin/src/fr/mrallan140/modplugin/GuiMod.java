535cfd266d32fdd876c25f87702cb7499d186bf0
mrallan140 <allan2000@wanadoo.fr>
2017-01-25 22:23:08 +0100

    Base

---
+++ ModPlugin/src/fr/mrallan140/modplugin/GuiMod.java
@@ -0,0 +1,13 @@
+package fr.mrallan140.modplugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class GuiMod implements Listener {
+
+	public GuiMod(Main main) {}
+	
+	@EventHandler
+	public void 
+
+}
--- ModPlugin/src/fr/mrallan140/modplugin/Main.java
+++ ModPlugin/src/fr/mrallan140/modplugin/Main.java
@@ -1,27 +1,21 @@
 package fr.mrallan140.modplugin;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
     public void onEnable(){
-        System.out.println("Plugin Moderation ON !");
+        	
+    	PluginManager pm = getServer().getPluginManager();
+    	pm.registerEvents(new GuiMod(this), this);
+    	
+    	System.out.println("Plugin Moderation ON !");
+        
     }
     
     public void onDisable(){
         System.out.println("Plugin Moderation OFF !");
     }
-    
-    public void onPlayerJoin(PlayerJoinEvent e) {
-       e.getJoinMessage();
-       e.getPlayer().sendMessage("MDR BIENVNUE");
-    }
-    
-    public void openGui(Player p){
-    	p.getPlayer().sendMessage("LOL");
-    	p.getType().getClass();
     	
-    }
 }
